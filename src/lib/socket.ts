8cc0026a008d5ba764ea6ac078c9073ee6e75bfa
Shahzad <your-email@example.com>
2026-06-23 23:22:49 +0500

    fix: configure production socket backend URL

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,5 +1,9 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001", {
-    transports: ["websocket"],
-});
+export const socket = io(
+    process.env.NEXT_PUBLIC_BACKEND_URL!,
+    {
+        transports: ["websocket"],
+        withCredentials: true,
+    }
+);
